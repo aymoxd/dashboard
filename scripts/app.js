eac623f7d6d66c853defd17a2ba2baccf0221cae
aymane-3azi <carlejohnsonbus@gmail.com>
2025-10-16 18:25:36 +0100

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+ const ctx = document.getElementById('salesChart');
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Jan', 'feb', 'mar', 'apr', 'may', 'jun','jul','aut','sep','oct',],
+      datasets: [{
+        label: 'Sales overview',
+        data: [5, 9, 7, 12, 15, 16,20,20,22,30],
+        borderWidth: 1,
+         borderColor: 'rgba(59, 130, 246, 1)',
+          backgroundColor: 'rgba(59, 130, 246, 0.2)',
+           fill: true,
+            tension: 0.4,
+             pointHoverRadius: 6,
+              pointRadius: 4,
+      }]
+    },
+    options: {
+         responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+   const ctx2 = document.getElementById('userChart').getContext('2d');
+  const userChart = new Chart(ctx2, {
+      type: 'doughnut',
+      data: {
+          labels: ['Morocco', 'USA', 'uk'],
+          datasets: [{
+              data: [100, 40, 30],
+              backgroundColor: [
+                  'rgba(59, 130, 246, 1)',
+                  'rgba(107, 114, 128, 1)',
+                  'rgba(156, 0, 175, 1)'
+              ]
+          }]
+      },
+      options: { cutout: '70%' }
+  });
